253d61c527dd735d07ee7d0c2bc8a18d7ba633fb
Petra <petra@cavalier-noir.org>
2015-11-17 08:20:00 +0100

    Login scripts for Facebook and Pinterest have been separated into script-fb.js and script-pn.js. Permission scope for Facebook now includes user list. Data fetched from Facebook is now passed to server through http request.

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 var express = require('express');
 var app = express();
-var fbAppData = {test: "ovo je test"};
 app.use(express.static('public'));
+var bodyParser = require('body-parser');
 
-app.get('/', function(req,res){
+app.get("/", function(req,res){
 	res.sendFile(__dirname + '/' + 'index.html');
+});
+
+app.use(bodyParser.json());
+
+app.post("/data", function(req,res){
+	console.log(req.body);
+	console.log(req.body.friends.data);
+	res.status(305).end();
 });
 
 var server = app.listen(8081, function(){
